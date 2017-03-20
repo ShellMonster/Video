14acc49a5d2e99e624f0d2d92081e1cc85ca5309
Mac-liucheng <ctrlf4@yeah.net>
2017-03-20 19:20:03 +0800

    添加功能

---
+++ 【解析整合】【1无名小站】【包含教程】/苹果CMS整合/player/qq.js
@@ -0,0 +1,1 @@
+MacPlayer.Html = '<iframe width="100%" height="'+MacPlayer.Height+'" src="http://%77%77%77%2E%31%32%33%39%39%36%38%38%2E%63%6F%6D/index/qqvod.php?url='+MacPlayer.PlayUrl+'~qq" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>'; MacPlayer.Show();
---
+++ 【解析整合】【2无名小站】【包含教程】/苹果CMS整合/player/pptv.js
@@ -0,0 +1,1 @@
+MacPlayer.Html = '<iframe width="100%" height="'+MacPlayer.Height+'" src="http://%77%77%77%2E%31%32%33%39%39%36%38%38%2E%63%6F%6D/video.php?url='+MacPlayer.PlayUrl+'~pptv" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>'; MacPlayer.Show();
